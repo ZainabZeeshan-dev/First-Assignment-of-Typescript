4a87c373f93b33c68a9395078b90dba6fa95672a
ZainabZeeshan-dev <125381822+ZainabZeeshan-dev@users.noreply.github.com>
2024-03-06 17:02:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//data types
+//string
+var studentname = "Zainab";
+console.log(studentname);
+//number
+var studentage = "18";
+console.log(studentage);
+//boolean
+var isstudent = true;
+console.log(isstudent);
+// Null
+var empty = null;
+console.log(empty);
+//Undefined
+var divide = undefined;
+console.log(divide);
+//symbol
+var s1 = Symbol();
+console.log(s1);
+//bigint
+var x = 234568765;
+var y = 1245789054;
+console.log(x + y);
---
+++ main.ts
@@ -0,0 +1,27 @@
+//data types
+//string
+let studentname: string ="Zainab";
+console.log(studentname);
+
+//number
+let studentage:number = "18";
+console.log(studentage);
+//boolean
+let isstudent : boolean = true;
+console.log(isstudent);
+// Null
+let empty = null;
+console.log(empty);
+
+//Undefined
+let divide = undefined;
+console.log(divide);
+
+//symbol
+let s1:symbol=Symbol();
+console.log(s1);
+//bigint
+let x =234568765;
+let y= 1245789054;
+console.log(x+y);
+
